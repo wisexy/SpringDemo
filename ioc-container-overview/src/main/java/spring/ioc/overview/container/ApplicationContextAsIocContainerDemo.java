abe6e697f592cfbd6945f5c105dfae1373396cc6
陈旻 <min@chenmindeMacBook-Pro.local>
2021-10-06 00:06:00 +0800

    add demo for ApplicationContext and BeanFactory.

---
+++ ioc-container-overview/src/main/java/spring/ioc/overview/container/ApplicationContextAsIocContainerDemo.java
@@ -0,0 +1,38 @@
+package spring.ioc.overview.container;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import spring.ioc.overview.domain.User;
+
+import java.util.Map;
+
+@Slf4j
+public class ApplicationContextAsIocContainerDemo {
+
+    public static void main(String args[]) {
+
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+
+        String location = "classpath:/META-INF/dependency-lookup-context.xml";
+
+        int beanCount = reader.loadBeanDefinitions(location);
+
+        log.info("Bean count = {}", beanCount);
+
+        lookupCollectionByType(beanFactory);
+
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
+            log.info("通过类型查找集合实例：{}", users.toString());
+        }
+    }
+}
